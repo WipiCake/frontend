6efaddd00b893a2e6fcb17d7dc1478cc70e59065
asdknkwqndknqkfw <codemonkey99999@gmail.com>
2025-03-31 21:56:06 +0900

    feat:footer 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import './App.css'
-
+import React from 'react';
+import HeaderPage from './components/Main/Header';
+import MainContent from './components/Main/Main';
+import FooterPage from './components/Main/Footer';
+import './App.css';
+import { Route, Routes } from 'react-router';
+import Footer from './components/Main/Footer';
+import MainPage from './page/MainPage';
 function App() {
-  
-
   return (
-    <>
-      <h1>React App</h1>
-        
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <>
+      <div className="bg-blue-500 h-[60vh]">Main</div>
+    </>
+  );
+};
+
+export default Main; 
