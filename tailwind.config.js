d191d83b2c521a1685f42ee2ed3b523a50fbb197
Nam Min Ki <nam960216@gmail.com>
2025-04-23 23:44:33 +0900

    chore: 환경설정 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
-import daisyui from "daisyui";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      fontFamily: ["Pretendard Variable", "Roboto"],
+      fontFamily: ['Pretendard Variable', 'Roboto'],
     },
   },
-  plugins: [
-    daisyui, // DaisyUI 플러그인 추가
-  ],
 };
