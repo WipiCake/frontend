2ba4b0d3525cd7a6e842b278fedade4663df4c7e
Nam Min Ki <nam960216@gmail.com>
2025-04-25 18:05:39 +0900

    chore: 글꼴 설치 및 reset css 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import React from "react";
-
-import { Route, Routes } from "react-router";
-
-import Contents from "./components/body/Contents";
-import Join from "./pages/join/Join";
-import Login from "./pages/login/Login";
-import FindId from "./pages/find-id/FindId";
-import FindIdCommonForm from "./pages/find-id/FindIdCommonForm";
-import FindIdVerificationCode from "./pages/find-id/FindIdVerificationCode";
-import FindIdCompleted from "./pages/find-id/FindIdCompleted";
-import MainContent from "./components/body/MainContent";
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import Contents from './components/body/Contents';
+import Join from './pages/join/Join';
+import Login from './pages/login/Login';
+import FindId from './pages/find-id/FindId';
+import FindIdCommonForm from './pages/find-id/FindIdCommonForm';
+import FindIdVerificationCode from './pages/find-id/FindIdVerificationCode';
+import FindIdCompleted from './pages/find-id/FindIdCompleted';
+import MainContent from './components/body/MainContent';
+import Mypage from './pages/mypage';
+import './global.css';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Contents />}>
-          <Route path="" element={<MainContent />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/find-id" element={<FindIdCommonForm />}>
-            <Route path="" element={<FindId />} />
-            <Route
-              path="verification-code"
-              element={<FindIdVerificationCode />}
-            />
-            <Route path="completed" element={<FindIdCompleted />} />
-          </Route>
+    <Routes>
+      <Route path="/" element={<Contents />}>
+        <Route path="" element={<MainContent />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/find-id" element={<FindIdCommonForm />}>
+          <Route path="" element={<FindId />} />
+          <Route
+            path="verification-code"
+            element={<FindIdVerificationCode />}
+          />
+          <Route path="completed" element={<FindIdCompleted />} />
         </Route>
-      </Routes>
-    </div>
+        <Route path="/mypage" element={<Mypage />} />
+      </Route>
+    </Routes>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,126 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+const config = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      fontFamily: ['Pretendard Variable', 'Roboto'],
+      fontFamily: {
+        customFont: ['CustomFont', 'sans-serif'],
+      },
+      fontSize: {
+        // Base font size for rem calculations
+        root: '10px',
+        // Pretendard Variable 한글
+
+        'k-13-Regular': [
+          '1.3rem',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: '0.1rem' },
+        ],
+        'k-13-Medium': [
+          '1.3rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-14-Regular': [
+          '1.4rem',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-14-Medium': [
+          '1.4rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-15-Regular': [
+          '1.5rem',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-16-Regular': [
+          '1.6rem',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-16-Medium': [
+          '1.6rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-17-Semibold': [
+          '1.7rem',
+          { fontWeight: 600, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-18-Medium': [
+          '1.8rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-20-Medium': [
+          '2rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-20-Bold': [
+          '2rem',
+          { fontWeight: 700, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-24_Regular': [
+          '2.4rem',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-28_Medium': [
+          '2.8rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'k-30-Light': [
+          '3rem',
+          { fontWeight: 300, lineHeight: '150%', letterSpacing: 0 },
+        ],
+
+        // Roboto 영문/숫자
+
+        'en-13-Regular': [
+          '1.3rem',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-13-Medium': [
+          '1.3rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-14-Regular': [
+          '1.4rem',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-14-Medium': [
+          '1.4rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-15-Regular': [
+          '1.5rem',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-16-Regular': [
+          '1.6rem',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-16-Medium': [
+          '1.6rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-17-Semibold': [
+          '1.7rem',
+          { fontWeight: 600, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-18-Medium': [
+          '1.8rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-24-Regular': [
+          '2.4rem',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-28-Medium': [
+          '2.8rem',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        'en-30-Light': [
+          '3rem',
+          { fontWeight: 300, lineHeight: '150%', letterSpacing: 0 },
+        ],
+      },
     },
   },
+  plugins: [],
 };
+
+export default config;
