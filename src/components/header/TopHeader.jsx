ecf6464cf3890db0f9c1649a307b6b4e26971e0f
unknown <devhsj0737@gmail.com>
2025-04-29 18:31:18 +0900

    style: 헤더 스타일 수정

--- src/components/header/TopHeader.jsx
+++ src/components/header/TopHeader.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Join from './../../pages/join/Join';
+import Login from './../../pages/login/Login';
 
 const TopHeader = () => {
   return (
-    <div className="w-full border-b border-[#CBCBCB] h-[47px]  top-0 flex justify-center bg-white">
-      <div className="flex min-w-[1280px] justify-end  items-center ">
-        <ul className="flex items-center  gap-[40px]">
-          <li className="relative after:content-['|'] after:absolute after:right-[-20px]">
-            <a href="#">회원가입</a>
+    <div className="top-0 flex h-[47px] w-full justify-center border-b border-[#CBCBCB] bg-white">
+      <div className="flex min-w-[1280px] items-center justify-end">
+        <ul className="flex items-center gap-[20px]">
+          <li className="relative flex items-center gap-[20px]">
+            <Link to="/join" className="text-k-15-Regular text-[#B56D6D]">
+              회원가입
+            </Link>
+            <span className="h-[15px] border-r border-[#DBDBDB]"></span>
           </li>
-          <li className="relative after:content-['|'] after:absolute after:right-[-20px]">
-            <a href="#">로그인</a>
+          <li className="relative flex items-center gap-[20px]">
+            <Link to="/login" className="text-k-15-Regular">
+              로그인
+            </Link>
+            <span className="h-[15px] border-r border-[#DBDBDB]"></span>
           </li>
           <li>
-            <a href="#">고객센터</a>
+            <a className="text-k-15-Regular">고객센터</a>
           </li>
         </ul>
       </div>
