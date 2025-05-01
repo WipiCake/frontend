98aef6b597f15688c293127fa314939f36ef1a83
Nam Min Ki <nam960216@gmail.com>
2025-05-01 22:58:00 +0900

    feat: 페이지 이동 시에 스크롤 상단에 위치하기 위한 컴포넌트 제작

--- src/components/mypage/MypageCategoryCard.jsx
+++ src/components/mypage/MypageCategoryCard.jsx
@@ -4,10 +4,10 @@ const MypageCategoryCard = ({ icon, title, content, path }) => {
   return (
     <Link
       to={path}
-      className="group flex w-[30.5rem] cursor-pointer items-center justify-center border border-[#DBDBDB] bg-white hover:border-[#505050]"
+      className="group flex w-[30.5rem] cursor-pointer items-center justify-center border border-black-20 bg-white hover:border-black-40"
     >
       <div className="flex h-[15.3rem] w-[24.3rem] flex-col items-center">
-        <div className="text-black-50 h-[4.4rem] w-[4.4rem] group-hover:text-pink-50">
+        <div className="h-[4.4rem] w-[4.4rem] text-black-50 group-hover:text-pink-50">
           {icon}
         </div>
         <span className="mt-[2.4em] text-k-18-Medium">{title}</span>
