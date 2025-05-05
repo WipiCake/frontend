1db5ca224ecc12bf54e2c2fc1dcae539a035801c
Nam Min Ki <nam960216@gmail.com>
2025-05-05 15:24:13 +0900

    style: 폰트 추가 및 관련 코드 수정

--- src/components/mypage/MypageOrderStatusCard.jsx
+++ src/components/mypage/MypageOrderStatusCard.jsx
@@ -3,8 +3,7 @@ const MypageOrderStatusCard = ({ title, count, className }) => {
     <div className="flex w-[25.6rem] items-center justify-center">
       <div className="flex h-[13.6rem] flex-col justify-between">
         <span className={`text-k-18-Medium ${className || ''}`}>{title}</span>
-        {/* 폰트 변경 필요 */}
-        <span className="text-en-24-Regular text-center">{count}</span>
+        <span className="text-k-24_Medium text-center">{count}</span>
       </div>
     </div>
   );
