16cbbce31bbbbc41833fe4eb93f1fb23e9ad306d
Nam Min Ki <nam960216@gmail.com>
2025-05-15 16:00:52 +0900

    feat: 배송지 관리 페이지 기능 제작

--- src/components/mypage/MypageEmptyState.jsx
+++ src/components/mypage/MypageEmptyState.jsx
@@ -1,10 +1,15 @@
-const MypageEmptyState = ({ icon, contents }) => {
+const MypageEmptyState = ({ icon, contents, isButton }) => {
   return (
-    <div className="bg-bg-10 mt-[2rem] flex h-[58rem] w-[128rem] flex-col items-center justify-center">
+    <div className="mt-[2rem] flex h-[58rem] w-[128rem] flex-col items-center justify-center bg-bg-10">
       <div className="flex h-[18.2rem] flex-col items-center justify-between">
         {icon}
-        <span className="text-black-40 text-k-28_Medium">{contents}</span>
+        <span className="text-k-28_Medium text-black-40">{contents}</span>
       </div>
+      {isButton && (
+        <button className="mt-[10.2rem] h-[5.7rem] w-[26.5rem] bg-pink-30 text-k-16-Regular text-bg-10">
+          배송지 등록
+        </button>
+      )}
     </div>
   );
 };
