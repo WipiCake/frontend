2d3fe35f68f8954524b56e6b69c90cf37f51b28d
Nam Min Ki <nam960216@gmail.com>
2025-05-25 14:51:35 +0900

    fix: 주문내역조회,찜한상품 페이지 제작 및 개인정보수정 페이지 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ScrollToTop from './components/common/ScrollToTop';
 import { Route, Routes } from 'react-router';
 import Contents from './components/body/Contents';
 import MainContent from './components/body/MainContent';
@@ -8,10 +9,12 @@ import FindId from './pages/find-id/FindId';
 import FindIdVerificationCode from './pages/find-id/FindIdVerificationCode';
 import FindIdCompleted from './pages/find-id/FindIdCompleted';
 import Mypage from './pages/mypage';
+import VerifyPage from './pages/mypage/VerifyPage';
+import EditPage from './pages/mypage/EditPage';
 import WishlistPage from './pages/mypage/WishlistPage';
+import CancelReturnPage from './pages/mypage/CancelReturnPage';
 import OrdersPage from './pages/mypage/OrdersPage';
 import AddressPage from './pages/mypage/AddressPage';
-import ScrollToTop from './components/common/ScrollToTop';
 import './global.css';
 
 function App() {
@@ -32,7 +35,10 @@ function App() {
             <Route path="completed" element={<FindIdCompleted />} />
           </Route>
           <Route path="/mypage" element={<Mypage />} />
+          <Route path="/mypage/veryify" element={<VerifyPage />} />
+          <Route path="/mypage/edit" element={<EditPage />} />
           <Route path="/mypage/wishlist" element={<WishlistPage />} />
+          <Route path="/mypage/cancel-return" element={<CancelReturnPage />} />
           <Route path="/mypage/orders" element={<OrdersPage />} />
           <Route path="/mypage/address" element={<AddressPage />} />
         </Route>
